d60dfe24b146e463080268db2a264849a3066eeb
jkpark104 <jkrang104@gmail.com>
2021-07-23 16:40:35 +0900

    feat: Create function to solve the problem

---
+++ fibo.py
@@ -0,0 +1,13 @@
+def chicken(x):
+    if x == 0: 
+      return 0
+    if x == 1: 
+      return 1
+    a,b = 1,1
+    while 1:
+        c = a+b
+        if c > x: 
+          return a + chicken(x-b)
+        elif c == x: 
+          return b
+        a, b = b, c
